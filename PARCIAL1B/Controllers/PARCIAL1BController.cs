73c8a9cbc45a8575481c8c1a4259ed25e7ff9472
oscar <oscar.martz511@gmail.com>
2024-03-09 11:52:17 -0600

    Se agrego modificar y borrar para platos

--- PARCIAL1B/Controllers/PARCIAL1BController.cs
+++ PARCIAL1B/Controllers/PARCIAL1BController.cs
@@ -22,10 +22,11 @@ namespace PARCIAL1B.Controllers
 
         public IActionResult Get()
         {
-            List<Platos> ListadoPlatos = ( from p in _pContex.platos
-                                           select p).ToList();
+            List<Platos> ListadoPlatos = (from p in _pContex.platos
+                                          select p).ToList();
 
-            if (ListadoPlatos.Count() == 0) {
+            if (ListadoPlatos.Count() == 0)
+            {
                 return NotFound();
             }
 
@@ -50,6 +51,53 @@ namespace PARCIAL1B.Controllers
         }
         //Fin de crear para platos
 
+        //Modificar platos
+        [HttpPut]
+        [Route("ActualizarPlato/{id}")]
+        public IActionResult ActualizarPlato(int id, [FromBody] Platos platoModificar)
+        {
+            Platos? platoActual = (from p in _pContex.platos
+                                   where p.PlatoID == id
+                                   select p).FirstOrDefault();
+            if (platoActual == null)
+            {
+                return NotFound();
+            }
 
+            platoActual.PlatoID = platoModificar.PlatoID;
+            platoActual.EmpresaID = platoModificar.EmpresaID;
+            platoActual.GrupoID = platoModificar.GrupoID;
+            platoActual.NombrePlato = platoModificar.NombrePlato;
+            platoActual.DescripcionPlato = platoModificar.DescripcionPlato;
+            platoActual.Precio = platoModificar.Precio;
+
+            _pContex.Entry(platoActual).State = EntityState.Modified;
+            _pContex.SaveChanges();
+
+            return Ok(platoModificar);
+        }
+        //Fin de modificar platos
+
+        //Borrar platos
+        [HttpDelete]
+        [Route("EliminarPlato/{id}")]
+        public IActionResult EliminarPlato(int id)
+        {
+            Platos? plato = (from p in _pContex.platos
+                             where p.PlatoID == id
+                             select p).FirstOrDefault();
+            if (plato == null)
+            {
+                return NotFound();
+            }
+
+            _pContex.platos.Attach(plato);
+            _pContex.platos.Remove(plato);
+            _pContex.SaveChanges();
+
+            return Ok(plato);
+            //Fin de borrar platos
+
+        }
     }
 }
